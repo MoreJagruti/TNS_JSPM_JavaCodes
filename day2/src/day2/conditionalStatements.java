3de4f3f4b11140f23158b1c5e9fc73b3a61ce5d1
Admin <Admin@DESKTOP-NKV5A7H>
2025-07-02 09:47:26 +0530

    Switch case statement.

--- day2/src/day2/conditionalStatements.java
+++ day2/src/day2/conditionalStatements.java
@@ -40,5 +40,36 @@ public class conditionalStatements {
 		else {
 			System.out.println("you do not fit in the age group to donate blood.");
 		}
+		
+		
+//		Switch case statements
+		int day = 2;
+		switch(day) {
+		
+		case 1:
+			System.out.println("Monday");
+			break;
+		case 2:
+			System.out.println("Tuesday");
+			break;
+		case 3:
+			System.out.println("Wednesday");
+			break;
+		case 4:
+			System.out.println("Thursday");
+			break;
+		case 5:
+			System.out.println("Fridayy");
+			break;
+		case 6:
+			System.out.println("Saturday");
+			break;
+		case 7:
+			System.out.println("Sunday");
+			break;
+		default:
+			System.out.println("Invalid choice");
+			
+		}
 	}
 }
