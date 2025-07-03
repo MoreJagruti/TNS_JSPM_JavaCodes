68ac4c8e5f57b25b430b71bd99e7f07a6fe7f125
Admin <Admin@DESKTOP-NKV5A7H>
2025-07-03 11:41:11 +0530

    Assignment 1 and 2

---
+++ Assignments/src/com/dailyAssignments/Assignment2ATMSys.java
@@ -0,0 +1,56 @@
+package com.dailyAssignments;
+import java.util.Scanner;
+public class Assignment2ATMSys {
+	float balance = 100000f;
+	public  float  checkBalance() {
+		return balance;
+	}
+	
+	public void Withdraw(float amount) {
+		if(amount<= balance) {
+			balance-=amount;
+			System.out.println("withdrawed "+ amount + "rs successfully.");
+		}
+		else {
+			System.out.println("Insufficient balance.");
+		}
+	}
+	
+	public void depositmoney(float amount) {
+		balance+=amount;
+		System.out.println("Amount deposit successful.");
+	}
+	public static void main(String[] args) {
+		float amount;
+		Assignment2ATMSys obj1 = new Assignment2ATMSys();
+		Scanner sc = new Scanner(System.in);
+		boolean again = true;
+		while(again) {
+			
+			int choice;
+			System.out.println("Enter your Choice:\n1.Check balance\n2.Withdraw cash\n3.Deposit money");
+			choice = sc.nextInt();
+			switch(choice) {
+			case 1:
+				System.out.println(obj1.checkBalance());
+				break;
+			case 2:
+				System.out.println("Enter amount to be withdrawed");
+				amount = sc.nextFloat();
+				obj1.Withdraw(amount);
+				break;
+			case 3:
+				System.out.println("Enter amount to be deposited");
+				amount = sc.nextInt();
+				obj1.depositmoney(amount);
+				break;
+			default:
+				System.out.println("Invalid choice");
+			}
+			
+			System.out.println("Do you want to continue ? (true/false)");
+			again = sc.nextBoolean();		}
+		
+	}
+
+}
